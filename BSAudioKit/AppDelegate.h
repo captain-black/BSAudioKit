38560111d7c6f4cbc3c32cb0b3e2119c5496e6ac
Captain Black <captainblack.soul@gmail.com>
2016-11-18 15:21:26 +0800

    Initial Commit

---
+++ BSAudioKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSAudioKit
+//
+//  Created by Captain Black on 2016/11/18.
+//  Copyright © 2016年 Captain Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSAudioKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSAudioKit
+//
+//  Created by Captain Black on 2016/11/18.
+//  Copyright © 2016年 Captain Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
